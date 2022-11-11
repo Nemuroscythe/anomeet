6b2562ac445b932f1c0d73ebc8192877fa109d1c
JeremyA <58225415+Nemuroscythe@users.noreply.github.com>
2022-11-11 13:58:42 +0100

    AM35 Ajout d'un service dans le flow

--- main/hello/controller/helloController.py
+++ main/hello/controller/helloController.py
@@ -1,10 +1,15 @@
 from flask import (
     Blueprint
 )
+
+from main.hello.service import helloService
+
 # Créer un blueprint que l'on doit lié a notre app instance dans __init__
 bp = Blueprint('hello', __name__, url_prefix='/')
 
+# Ce fichier contient tout nos points d'entrées (endpoints) pour la partie "hello"
+
 # Une simple page sur /hello HTTP method par défaut : GET
 @bp.route('/hello')
-def hello():
-    return 'Hello, World!'
+def getHello():
+    return helloService.getHelloMessage()
---
+++ main/hello/service/helloService.py
@@ -0,0 +1,4 @@
+# Ce fichier contient notre logique et manipulations de données
+
+def getHelloMessage():
+    return 'Hello, World!'
