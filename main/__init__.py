11b93488cd4277c074a95c0407254480bd9d207a
JeremyA <58225415+Nemuroscythe@users.noreply.github.com>
2022-11-12 13:27:10 +0100

    AM35 Autoriser l'accès via react (localhost:3000)
    Ajout de flask-cors
    Ajout d'un CORS avec pour origin notre addresse local react

--- main/__init__.py
+++ main/__init__.py
@@ -1,15 +1,19 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
-# create and configure the app
+# Crée un instance d'application
 app = Flask(__name__, instance_relative_config=True)
 
-# load the instance config
+# Charge la configuration qui se trouve config.py
 app.config.from_object('main.config')
 app.logger.debug(app.config.get('SQLALCHEMY_DATABASE_URI'))
 
-database = SQLAlchemy(app)
+# Autorise les autres applications à l'appeler
+# si l'addresse d'origine commence par http://localhost:3000
+cors = CORS(app, origins=["http://localhost:3000"])
 
+database = SQLAlchemy(app)
 database.init_app(app)
 
 from .hello.controller.helloController import bp
