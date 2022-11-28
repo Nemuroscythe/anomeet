15fbc390e8cd883267931b046127577c6b3347ae
JeremyA <58225415+Nemuroscythe@users.noreply.github.com>
2022-11-28 18:33:08 +0100

    AM6 Ajout de sexe, ajout de décorator et descriptor pour la validation

---
+++ backend/test/anomeet_backend_test.py
@@ -0,0 +1,19 @@
+import pytest
+from backend.users import Users
+
+
+def test_last_name_empty():
+    with pytest.raises(Exception) as e_info:
+        last_name = ""
+        first_name = "Doe"
+        email = "JohnDoe@anonymous.com"
+        password = "JohnDoe007"
+        Users(last_name, first_name, email, password)
+
+def test_email_invalid():
+    with pytest.raises(Exception) as e_info:
+        last_name = "John"
+        first_name = "Doe"
+        email = "John.anonymous.com"
+        password = "JohnDoe007"
+        Users(last_name, first_name, email, password)
--- backend/users.py
+++ backend/users.py
@@ -1,6 +1,42 @@
+import re
+
+
+class Email:
+    def __get__(self, obj, objtype=None):
+        return self.value
+
+    def __set__(self, obj, value):
+        regex="^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$"
+        if not re.match(regex, value):
+            raise ValueError("Votre email n'est pas valide")
+        self.value = value
+
+def email(attr):
+    def decorator(classe):
+        setattr(classe, attr, Email())
+        return classe
+    return decorator
+#
+# def textNotBlank(attr):
+#     def decorator(classe):
+#         setattr(classe, attr, TextNotBlank())
+#         return classe
+#     return decorator
+#
+# def password(attr):
+#     def decorator(classe):
+#         setattr(classe, attr, Password())
+#         return classe
+#     return decorator
+
+@email(email)
+# @textNotBlank(first_name)
+# @textNotBlank(last_name)
+# @password(password())
 class Users:
-    def __init__(self, first_name, last_name, email, password):
+    def __init__(self, first_name, last_name, email, password, sexe):
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
         self.password = password
+        self.sexe = sexe
