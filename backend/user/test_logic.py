44461193de01d845e43fb6a1a593206d8c8735bc
ali-duman <ali-duman43@hotmail.com>
2023-01-16 20:38:10 +0100

    AM 7 - Avancement de test unit

--- backend/user/test_logic.py
+++ backend/user/test_logic.py
@@ -1,24 +1,55 @@
 import pytest
 from logic import *
 
+# Si ça fonctionne
+# Si ne fonctionne pas
 
-def test_check_name():
+
+def test_check_name_when_true_length_50():
     assert check_name("Jean-Mich", "Dupont") == True
+
+
+def test_check_name_when_false_length_50():
+    assert check_name("Jean-MichJean-MichJean-MichJean-MichJean-MichJean-MichJean-Mich", "DupontDupontDupontDupontDupontDupontDupontDupontDupont") == False
+
+
+def test_check_name_when_true_empty():
+    assert check_name("Jean-Mich", "Dupont") == True
+
+
+def test_check_name_when_false_empty():
     assert check_name(" ", "") == False
 
 
-def test_check_email():
+def test_check_name_when_true_isspace():
+    assert check_name("Jean-Mich", "Dupont") == True
+
+
+def test_check_name_when_false_isspace():
+    assert check_name(" ", " ") == False
+
+
+def test_check_email_when_true_regex():
     assert check_email("jean-mich@dupont.fr") == True
+
+
+def test_check_email_when_false_regex():
     assert check_email("jeanmich.fr") == False
 
 
-def test_check_password():
+def test_check_password_when_true_regex():
     assert check_password("Azertyuiop123$") == True
+
+
+def test_check_password_when_false_regex():
     assert check_password("Azertyuiop123") == False
 
 
 def test_check_if_same_password():
     assert check_if_same_password("bonmdp", "bonmdp") == True
+
+
+def test_check_if_same_password():
     assert check_if_same_password("bonmdp", "mauvaismdp") == False
 
 
