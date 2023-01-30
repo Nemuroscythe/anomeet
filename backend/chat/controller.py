434d65775fb7ed2753ca8146dfc8e9f54746b76b
Nemuroscythe <58225415+Nemuroscythe@users.noreply.github.com>
2023-01-30 20:30:12 +0100

    AM9 Vérification de l'existence des utilisateurs + affichages des noms

--- backend/chat/controller.py
+++ backend/chat/controller.py
@@ -41,6 +41,20 @@ def am38_js():
     return Response(js, mimetype='text/javascript')
 
 
-@blueprint.route("/start_conversation/<user_id>/<user_2_id>", methods=["GET"])
-def create_random_conversation(user_id, user_2_id):
-    return user_id + " " + user_2_id
+@blueprint.route("/start_conversation/<user_one_id>/<user_two_id>", methods=["GET"])
+def create_random_conversation(user_one_id, user_two_id):
+    user_one_fullname = get_user_fullname(user_one_id)
+    user_two_fullname = get_user_fullname(user_two_id)
+    return f"{user_one_fullname} and {user_two_fullname} exists"
+
+
+def get_user_fullname(user_id):
+    psycopg2_connection_string = current_app.config.get("PSYCOPG2_CONNECTION_STRING")
+    try:
+        with psycopg2.connect(psycopg2_connection_string) as conn:
+            with conn.cursor() as cur:
+                cur.execute("SELECT first_name, last_name FROM users WHERE id = %s;", (user_id,))
+                user_data = cur.fetchone()
+                return user_data[0] + " " + user_data[1]
+    except Exception as e:
+        return f"User with id: {user_id} does not exist {str(e)}"
