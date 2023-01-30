d7eb19e071678f3a8358936df627fcc198c0139f
ali-duman <ali-duman43@hotmail.com>
2023-01-30 20:03:38 +0100

    AM 46 - Récupérer les données de la BDD avec Python - Création de la route recuperer_utilisateur et affichage des données dans show_user.html

--- backend/user/controller.py
+++ backend/user/controller.py
@@ -1,7 +1,7 @@
 # Definition des routes
 # if se trouve dans controller
 import psycopg2
-from flask import Blueprint, request, current_app
+from flask import Blueprint, request, current_app, render_template
 
 from .logic import check_user_signup
 
@@ -10,26 +10,18 @@ blueprint = Blueprint('user', __name__, url_prefix='/')
 
 @blueprint.route("/recuperer_utilisateur", methods=["GET"])
 def recuperer_utilisateur():
-    sql = """INSERT INTO users(first_name, last_name, email, password, sex)
-         VALUES(%s,%s,%s,%s,%s);"""
-    try:
-        psycopg2_connection_string = current_app.config.get("PSYCOPG2_CONNECTION_STRING")
-        # connect to the PostgreSQL database
-        conn = psycopg2.connect(psycopg2_connection_string)
-        # create a new cursor
-        cur = conn.cursor()
-        # execute the INSERT statement
-        cur.execute(sql, (first_name, last_name, email, password, sex))
-        # commit the changes to the database
-        conn.commit()
-        # close communication with the database
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-    return "Utilisateur créé !"
+    psycopg2_connection_string = current_app.config.get("PSYCOPG2_CONNECTION_STRING")
+    # connect to the PostgreSQL database
+    conn = psycopg2.connect(psycopg2_connection_string)
+    # create a new cursor
+    cur = conn.cursor()
+    # execute the SELECT statement
+    cur.execute('SELECT * FROM users;')
+    # fetch the data
+    result = cur.fetchall()
+    cur.close()
+    conn.close()
+    return render_template("user/show_user.html", result=result)
 
 
 @blueprint.route("/creer_utilisateur", methods=["GET"])
