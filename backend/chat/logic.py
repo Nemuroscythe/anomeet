f09e6b87f72e780fa59d9fb61b894002cd267802
GuyGillain <115338293+GuyGillain@users.noreply.github.com>
2023-02-13 19:16:48 +0100

    logic.py

--- backend/chat/logic.py
+++ backend/chat/logic.py
@@ -1,3 +1,6 @@
+import json
+import psycopg2
+
 def verification_msg(msg):
     if msg == "" or len(msg) > 512:
         return False
@@ -5,4 +8,14 @@ def verification_msg(msg):
     return True
 
 def retrieveMsg(id_user, id_conversation):
-    sql = "SELECT id_person, id, content, date FROM message JOIN conversation ON "
+    sql = """SELECT id_person, id, content, date 
+             FROM message
+             WHERE id_conversation = %s """
+
+    conn = psycopg2.connect(current_app.config.get("PSYCOPG2_CONNECTION_STRING"))
+    cur = conn.cursor()
+    cur.execute(sql, (id_conversation, ))
+
+    data = cur.fetchall()
+    
+    return json.dumps(data)
