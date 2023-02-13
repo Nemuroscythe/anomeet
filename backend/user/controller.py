cc999ac968725aa6877694f88d75e7dbc064e6d9
ali-duman <ali-duman43@hotmail.com>
2023-02-13 19:11:57 +0100

    AM 46 - Récupérer les données de la BDD avec Python - Suppression de cookies (/disconnect), gestions du comportement des différentes routes si l'utilisateur est connecté

--- backend/user/controller.py
+++ backend/user/controller.py
@@ -27,7 +27,7 @@ def recuperer_utilisateur():
 
 @blueprint.route("/creer_utilisateur", methods=["GET"])
 def creer_utilisateur():
-    if request.cookies.get('user_id'):
+    if not request.cookies.get('user_id'):
         if request.method == 'GET':
             last_name = request.args['last_name']
             first_name = request.args['first_name']
@@ -36,32 +36,32 @@ def creer_utilisateur():
             confirm_password = request.args['confirm_password']
             sex = request.args['sex']
 
-        if check_user_signup(first_name, last_name, email, password, confirm_password, sex):
-            sql = """INSERT INTO users(first_name, last_name, email, password, sex)
-                 VALUES(%s,%s,%s,%s,%s);"""
-            try:
-                psycopg2_connection_string = current_app.config.get("PSYCOPG2_CONNECTION_STRING")
-                # connect to the PostgreSQL database
-                conn = psycopg2.connect(psycopg2_connection_string)
-                # create a new cursor
-                cur = conn.cursor()
-                # execute the INSERT statement
-                cur.execute(sql, (first_name, last_name, email, password, sex))
-                # commit the changes to the database
-                conn.commit()
-                # close communication with the database
-                cur.close()
-            except (Exception, psycopg2.DatabaseError) as error:
-                print(error)
-            finally:
-                if conn is not None:
-                    conn.close()
+            if check_user_signup(first_name, last_name, email, password, confirm_password, sex):
+                sql = """INSERT INTO users(first_name, last_name, email, password, sex)
+                     VALUES(%s,%s,%s,%s,%s);"""
+                try:
+                    psycopg2_connection_string = current_app.config.get("PSYCOPG2_CONNECTION_STRING")
+                    # connect to the PostgreSQL database
+                    conn = psycopg2.connect(psycopg2_connection_string)
+                    # create a new cursor
+                    cur = conn.cursor()
+                    # execute the INSERT statement
+                    cur.execute(sql, (first_name, last_name, email, password, sex))
+                    # commit the changes to the database
+                    conn.commit()
+                    # close communication with the database
+                    cur.close()
+                except (Exception, psycopg2.DatabaseError) as error:
+                    print(error)
+                finally:
+                    if conn is not None:
+                        conn.close()
 
-            flash("Votre profil a bien été créé !", 'bg-success')
-            return render_template("user/connexion.html")
-        else:
-            flash("Il y a une erreur dans votre formulaire", 'bg-danger')
-            return render_template("user/sign_in.html")
+                flash("Votre profil a bien été créé !", 'bg-success')
+                return render_template("user/connexion.html")
+            else:
+                flash("Il y a une erreur dans votre formulaire", 'bg-danger')
+                return render_template("user/sign_in.html")
     else:
         flask.abort(403)
 
