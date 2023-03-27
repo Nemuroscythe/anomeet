f748cfa03afc80b577941862fdf87887979ffc87
Arnaud Mahieu <mahieuarnaud1909@hotmail.com>
2023-03-27 19:34:16 +0200

    merge main - prod

--- backend/index/controller.py
+++ backend/index/controller.py
@@ -7,17 +7,20 @@ blueprint = Blueprint('index', __name__, url_prefix='/')
 
 @blueprint.route("/")
 def index():
-    # Connexion à la base de données Alwaysdata
-    psycopg2_connection_string = current_app.config.get("PSYCOPG2_CONNECTION_STRING")
-    with psycopg2.connect(psycopg2_connection_string) as conn:
-        with conn.cursor() as cur:
-            cur.execute('SELECT * FROM "public"."hello";')
-            data = cur.fetchone()
-    # À partir d'ici data = une list contenant les infos reçues du serveur
-    # idéalement il faut faire un print de data --> print(data)
-    # pour voir comment les données sont organisées sur la table sql
-    # ici je sais qu'il me faut data[0]
-    return render_template("index.html")
+    if request.cookies.get('user_id'):
+        return render_template("homev2.html")
+    else:
+        # Connexion à la base de données Alwaysdata
+        psycopg2_connection_string = current_app.config.get("PSYCOPG2_CONNECTION_STRING")
+        with psycopg2.connect(psycopg2_connection_string) as conn:
+            with conn.cursor() as cur:
+                cur.execute('SELECT * FROM "public"."hello";')
+                data = cur.fetchone()
+        # À partir d'ici data = une list contenant les infos reçues du serveur
+        # idéalement il faut faire un print de data --> print(data)
+        # pour voir comment les données sont organisées sur la table sql
+        # ici je sais qu'il me faut data[0]
+        return render_template("index.html")
 
 
 
